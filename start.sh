9c066ff7822eaa88e9ebec577fc1389e5d6b1a61
ksh <7262ksh@gmail.com>
2023-11-21 05:22:30 +0000

    Edited start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
-#! /bin/bash
-sudo systemctl daemon-reload
-sudo systemctl enable --now node.service
-sudo systemctl status node.service
+cd /home/ec2-user/stupidscore/
+npm install
+cd /home/ec2-user/stupidscore/stupidscore-back
+npm install
+npm ci
+node ./bin/www
+#sudo systemctl daemon-reload
+#sudo systemctl enable --now node.service
+#sudo systemctl status node.service
