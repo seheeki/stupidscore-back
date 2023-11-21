7d0e4d5c7d75bfd5c4d1c2d772759c8febc2c5c9
ksh <7262ksh@gmail.com>
2023-11-21 06:47:53 +0000

    start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,7 @@ npm install
 cd /home/ec2-user/stupidscore/stupidscore-back
 npm install
 npm ci
-node ./bin/www
 #sudo systemctl daemon-reload
-#sudo systemctl enable --now node.service
+sudo systemctl enable --now node.service
 #sudo systemctl status node.service
 #tes
